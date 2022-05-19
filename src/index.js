dafe2f2e3a29746442caf671847be1b087f4658b
iavianm <iavianm@mail.ru>
2022-05-19 22:49:16 +0300

    nine step

--- src/index.js
+++ src/index.js
@@ -5,3 +5,18 @@ export const NOD = (x, y) => {
   if (!y) return x;
   return NOD(y, x % y);
 };
+
+export const primeNumber = (n) => {
+  if (n === 2) return 'yes';
+  if (n > 2) {
+    let i = 2;
+    const limit = Math.sqrt(n);
+    while (i <= limit) {
+      if (n % i === 0) {
+        return 'no';
+      }
+      i += 1;
+    }
+  }
+  return 'yes';
+};
