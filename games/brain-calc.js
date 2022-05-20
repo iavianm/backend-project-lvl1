d632e3256d1b1a79188fec1967c2b6773b3f4243
iavianm <iavianm@mail.ru>
2022-05-20 10:04:46 +0300

    refactor code'

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -8,13 +8,29 @@ console.log(`Hi ${userName}!`);
 console.log('What is the result of the expression?');
 
 for (let i = 1; i < 4; i += 1) {
-  const arr = ['+', '-', '*'];
-  const rand = getRandomInt(arr.length);
-  const rValue = arr[rand];
-  const question = (`${getRandomInt(10)} ${rValue} ${getRandomInt(10)}`);
+  const operations = ['+', '-', '*'];
+  const rand = getRandomInt(operations.length);
+  const operation = operations[rand];
+  const firstNumber = getRandomInt(10);
+  const secondNumber = getRandomInt(10);
+  const question = (`${firstNumber} ${operation} ${secondNumber}`);
   console.log(`Question: ${question}`);
-  const correctAnswer = `${eval(question)}`;
-  const yourAnswer = readlineSync.question('Your answer: ');
+
+  let correctAnswer = 0;
+  switch (operation) {
+    case '+':
+      correctAnswer = firstNumber + secondNumber;
+      break;
+    case '-':
+      correctAnswer = firstNumber - secondNumber;
+      break;
+    case '*':
+      correctAnswer = firstNumber * secondNumber;
+      break;
+    default:
+  }
+
+  const yourAnswer = Number(readlineSync.question('Your answer: '));
 
   if (yourAnswer === correctAnswer) console.log('Correct!');
   if (yourAnswer !== correctAnswer) {
