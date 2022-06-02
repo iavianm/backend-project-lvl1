a59cf2932ae0d73b9edee63ae1fea1f1a4a1441d
iavianm <iavianm@mail.ru>
2022-06-02 10:17:20 +0300

    fix cli / change the games directory

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('May I have your name? ');
-export default userName;
+const askName = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+};
+
+export default askName;
